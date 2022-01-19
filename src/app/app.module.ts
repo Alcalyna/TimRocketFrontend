6c04b3c07719f01c363b99158432778aa4a8cb3e
tom-scheyltjens <tom_scheyltjens@outlook.com>
2022-01-19 16:09:43 +0100

    feat(Story 1): basic implementation

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {LayoutModule} from "./layout/layout.module";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    NoopAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+
+  ],
+  providers: [],
+  exports: [
+    HomeComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/model/Member.ts
@@ -0,0 +1,11 @@
+
+export interface Member {
+
+  memberId : string
+  firstname : string
+  lastname : string
+  email : string
+  password : string
+  role : string
+  company : string
+}
