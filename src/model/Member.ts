19d52ebac569d824c01624ddf20ea6564207c1fa
Linh P. Nguyen <nguyenlp.info@gmail.com>
2022-01-20 10:13:05 +0100

    feat(Story 3): unique profile component

---
+++ src/model/Member.ts
@@ -0,0 +1,10 @@
+
+export interface Member {
+
+  memberId : string
+  firstname : string
+  lastname : string
+  email : string
+  role : string
+  company : string
+}
---
+++ src/service/member.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import { Observable } from 'rxjs';
+import {Member} from "../model/Member";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+
+  url: string
+
+  constructor(private http: HttpClient) {
+    this.url = `${environment.backendUrl}/members`;
+  }
+
+  getMembers(): Observable<Member[]> {
+    return this.http.get<any[]>(this.url);
+  }
+}
