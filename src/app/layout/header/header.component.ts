099f70034ab4841fdbdb50d30425e1b2794f138e
Linh P. Nguyen <nguyenlp.info@gmail.com>
2022-01-25 09:43:34 +0100

    feat(Story 3): trying to retrieve the logged member

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Member} from "../../../model/Member";
 import {KeycloakService} from "../../keycloak/keycloak.service";
 import {async, Observable, tap} from "rxjs";
+import {MemberService} from "../../../service/member.service";
 
 @Component({
   selector: 'app-header',
@@ -11,16 +12,20 @@ import {async, Observable, tap} from "rxjs";
 export class HeaderComponent implements OnInit {
 
   //loggedInUser : Member | undefined;
-  currentUser$! : Observable<Member>
+  currentUser$! : Observable<Member>;
 
   constructor(
-    private keyCloakService: KeycloakService
+    private keyCloakService: KeycloakService,
+    private memberService: MemberService
   ) { }
 
   ngOnInit(): void {
    // this.keyCloakService.currentMember.subscribe(member => this.loggedInUser = member);
-    this.currentUser$ = this.keyCloakService.currentMember.pipe(tap (user => console.log(" user logged in? " + user))) ;
-    console.log(this.currentUser$)
+   //  this.currentUser$ = this.keyCloakService.currentMember.pipe(tap (user => console.log(" user logged in? " + user))) ;
+   //  console.log(this.currentUser$)
+
+    // this.memberService.getMemberBy(this.keyCloakService.getUsername()).subscribe(user => this.currentUser$ = user);
+    this.currentUser$ = this.memberService.getMemberBy(this.keyCloakService.getUsername());
   }
 
 
