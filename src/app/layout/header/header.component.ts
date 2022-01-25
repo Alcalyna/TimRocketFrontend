b98b724ce481150a116271efe74bd7a8c5b68847
Linh P. Nguyen <nguyenlp.info@gmail.com>
2022-01-25 10:10:54 +0100

    feat(Story 3): displaying and hiding correct buttons when logged in

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -12,27 +12,53 @@ import {MemberService} from "../../../service/member.service";
 export class HeaderComponent implements OnInit {
 
   //loggedInUser : Member | undefined;
-  currentUser$! : Observable<Member>;
+//   currentUser$! : Observable<Member>;
+//
+//   constructor(
+//     private keyCloakService: KeycloakService,
+//     private memberService: MemberService
+//   ) { }
+//
+//   ngOnInit(): void {
+//    // this.keyCloakService.currentMember.subscribe(member => this.loggedInUser = member);
+//    //  this.currentUser$ = this.keyCloakService.currentMember.pipe(tap (user => console.log(" user logged in? " + user))) ;
+//    //  console.log(this.currentUser$)
+//
+//     // this.memberService.getMemberBy(this.keyCloakService.getUsername()).subscribe(user => this.currentUser$ = user);
+//     this.currentUser$ = this.memberService.getMemberBy(this.keyCloakService.getUsername());
+//   }
+//
+//
+//
+//   clickLogin() {
+//
+//   }
+//
+//
+// }
+  currentUser!: Member;
+  name: String = "abc";
 
   constructor(
     private keyCloakService: KeycloakService,
     private memberService: MemberService
-  ) { }
-
-  ngOnInit(): void {
-   // this.keyCloakService.currentMember.subscribe(member => this.loggedInUser = member);
-   //  this.currentUser$ = this.keyCloakService.currentMember.pipe(tap (user => console.log(" user logged in? " + user))) ;
-   //  console.log(this.currentUser$)
-
-    // this.memberService.getMemberBy(this.keyCloakService.getUsername()).subscribe(user => this.currentUser$ = user);
-    this.currentUser$ = this.memberService.getMemberBy(this.keyCloakService.getUsername());
+  ) {
   }
 
+  ngOnInit(): void {
+    // this.keyCloakService.currentMember.subscribe(member => this.loggedInUser = member);
+
+    // this.keyCloakService.currentMember.pipe(tap(() => console.log("debug")
+    // )).subscribe(user => this.currentUser = user);
+    // console.log(this.currentUser);
+    // console.log(this.keyCloakService);
 
 
+
+    this.memberService.getMemberBy(this.keyCloakService.getUsername()).subscribe(user => this.currentUser = user);
+  }
   clickLogin() {
 
   }
-
 
 }
