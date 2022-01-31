af85c33489e6ce565cc6474ae3438cf35d213721
Linh P. Nguyen <nguyenlp.info@gmail.com>
2022-01-31 12:50:38 +0100

    feat(Story 5): add routing

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../service/user.service";
 import {User} from "../../model/User";
 import {KeycloakService} from "../keycloak/keycloak.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-apply',
@@ -12,15 +13,16 @@ export class ApplyComponent implements OnInit {
 
   currentUser!: User;
 
-  constructor(private userService : UserService, private keyCloakService: KeycloakService) { }
+  constructor(private userService : UserService, private keyCloakService: KeycloakService, private router : Router) { }
 
   ngOnInit(): void {
     this.userService.getUserBy(this.keyCloakService.getUsername()).subscribe(user => this.currentUser = user);
   }
 
   becomeACoach() {
-    this.userService.editRoleToCoach(this.currentUser?.userId!).subscribe(res => {
-
-    });
+    /*this.userService.editRoleToCoach(this.currentUser?.userId!).subscribe(res => {
+         this.router.navigate(['coach/:{this.currentUser?.userId!}'])
+    });*/
+    this.router.navigate(['coach/:this.currentUser?.userId!'])
   }
 }
