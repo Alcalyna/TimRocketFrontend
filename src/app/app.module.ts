1675e0fd268fec760092a1682d027ced9c74a4a3
RubenNeven <rubenneven@gmail.com>
2022-02-01 16:24:10 +0100

    feat(story 13): finished request a session with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,10 +28,10 @@ import {ApplyComponent} from './apply/apply.component';
 import {RouterModule} from "@angular/router";
 import {CoachDetailComponent} from './coach-detail/coach-detail.component';
 import {FilterByExperiencePipe} from './pipes/filter-by-experience.pipe';
-import { RequestASessionComponent } from './request-a-session/request-a-session.component';
+import {RequestASessionComponent} from './request-a-session/request-a-session.component';
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
-import {DatePipe} from "@angular/common";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
@@ -70,10 +70,11 @@ import {DatePipe} from "@angular/common";
     MatCardModule,
     RouterModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatInputModule
 
   ],
-  providers: [  { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true }, DatePipe],
+  providers: [  { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true }],
     exports: [
         HomeComponent,
         FilterPipe,
--- src/app/request-a-session/request-a-session.component.ts
+++ src/app/request-a-session/request-a-session.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SessionService} from "../../service/session.service";
 import {UserService} from "../../service/user.service";
 import {User} from "../../model/User";
 import {Session} from "../../model/Session";
-import {DatePipe} from "@angular/common";
+import DatepickerOptions = M.DatepickerOptions;
+import {now} from "jquery";
 
 
 @Component({
@@ -17,43 +18,118 @@ export class RequestASessionComponent implements OnInit {
 
   public error: any;
   private user!: User;
+  datePickerElem: any;
+  timePickerElem: any;
 
-
-  sessionForm: FormGroup = this.formBuilder.group({
-    sessionId: '',
-    subject: '',
-    date: '',
-    time: '',
-    location: '',
-    f2fLocation: '',
-    remarks: ''
+  sessionForm: FormGroup =      this.formBuilder.group({
+    subject: ['', [Validators.required, Validators.maxLength(25)]],
+    date: ['', [Validators.required]],
+    time: ['', [Validators.required]],
+    location: ['', [Validators.required]],
+    f2fLocation: ['', [Validators.required]],
+    remarks: ['', [Validators.maxLength(250)]]
   });
 
   constructor(private formBuilder: FormBuilder,
               private router: Router,
               private activatedRoute: ActivatedRoute,
               private sessionService: SessionService,
-              private userService: UserService,
-              private datePipe: DatePipe) {
+              private userService: UserService) {
 
   }
 
   ngOnInit(): void {
     this.userService.getcurrentUser()
       .subscribe(user => this.user = user);
+    this.setValidators();
   }
 
-  ngAfterViewInit(){
-    $(document).ready(function () {
-      $('.datepicker').datepicker();
-      $('.timepicker').timepicker();
+  private setValidators() {
+    this.sessionForm.get('location')?.valueChanges.subscribe(
+      result => {
+        if (result === 'f2f') {
+          this.sessionForm.get('f2fLocation')?.setValidators(Validators.required);
+        } else {
+          this.sessionForm.get('f2fLocation')?.setValidators(null);
+        }
+        this.sessionForm.get('f2fLocation')?.updateValueAndValidity();
+      })
+  }
+
+  ngAfterViewInit() {
+    $(document).ready(() => {
+      this.initializeDatePicker();
+      this.initializeTimePicker();
+      $('select').formSelect();
     });
+
+  }
+
+  private initializeDatePicker(): void {
+    const elems = document.querySelectorAll('.datepicker');
+    const instances = M.Datepicker.init(elems, {
+      format: 'yyyy-mm-dd',
+      minDate: new Date(),
+      onClose: () => {
+        this.sessionForm.get('date')?.patchValue(this.datePickerElem.toString());
+        this.sessionForm.updateValueAndValidity({onlySelf: false, emitEvent: true});
+      }
+    } as DatepickerOptions);
+
+    for (const instance of instances) {
+      if (instance !== undefined) {
+        this.datePickerElem = instance;
+      }
+    }
+  }
+
+  private initializeTimePicker(): void {
+    const elems = document.querySelectorAll('.timepicker');
+    const instances = M.Timepicker.init(elems, {
+      'twelveHour': false,
+      'onCloseEnd': () => {
+        this.sessionForm.get('time')?.patchValue(this.timePickerElem.time);
+        this.sessionForm.updateValueAndValidity({onlySelf: false, emitEvent: true});
+      }
+    });
+    for (const instance of instances) {
+      if (instance !== undefined) {
+        this.timePickerElem = instance;
+      }
+    }
+  }
+
+  get subject(): FormControl {
+    return this.sessionForm.get('subject') as FormControl;
+  }
+
+  get date(): FormControl {
+    return this.sessionForm.get('date') as FormControl;
+  }
+
+  get remarks(): FormControl {
+    return this.sessionForm.get('remarks') as FormControl;
+  }
+
+  get time(): FormControl {
+    return this.sessionForm.get('time') as FormControl;
+  }
+
+  get location(): FormControl {
+    return this.sessionForm.get('location') as FormControl;
+  }
+
+  get f2fLocation(): FormControl {
+    return this.sessionForm.get('f2fLocation') as FormControl;
   }
 
 
   onSubmit() {
+
     const sessionToCreate = this.sessionForm.value as Session;
-    this.datePipe.transform(sessionToCreate.date, 'yyyy-MM-dd');
+    console.log(this.location.value);
+    console.log(this.subject.value);
+    console.log(sessionToCreate.date);
     sessionToCreate.coach_id = this.activatedRoute.snapshot.paramMap.get('id')!;
     sessionToCreate.coachee_id = this.user.userId;
     this.sessionService.createSession(sessionToCreate)
@@ -65,4 +141,6 @@ export class RequestASessionComponent implements OnInit {
         }
       );
   }
+
+
 }
