aea73c5ed53f881956e3887ec9fbbace622c4208
RubenNeven <rubenneven@gmail.com>
2022-02-01 10:20:29 +0100

    feat(story 13): added coach detail

--- src/app/coach-detail/coach-detail.component.ts
+++ src/app/coach-detail/coach-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Coach} from "../../model/Coach";
+import {UserService} from "../../service/user.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-coach-detail',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoachDetailComponent implements OnInit {
 
-  constructor() { }
+  coach: Coach | undefined
+
+  constructor(private userService: UserService,
+              private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.getUser()
+  }
+
+  getUser(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.userService.getCoach(id!)
+      .subscribe(coach => this.coach = coach);
   }
 
 }
