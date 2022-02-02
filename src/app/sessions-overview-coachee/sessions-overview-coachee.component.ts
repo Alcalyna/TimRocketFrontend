0336f732df4470726fb9e502838d979f36b43d3b
Léonie Bouchat <leonie_bouchat@yahoo.fr>
2022-02-02 14:10:54 +0100

    feat(story 14): filter for coachee id

--- src/app/sessions-overview-coachee/sessions-overview-coachee.component.ts
+++ src/app/sessions-overview-coachee/sessions-overview-coachee.component.ts
@@ -4,6 +4,7 @@ import {Session} from "../../model/Session";
 import {Observable, Subscription} from "rxjs";
 import {SessionService} from "../../service/session.service";
 import {User} from "../../model/User";
+import {KeycloakService} from "../keycloak/keycloak.service";
 
 @Component({
   selector: 'app-sessions-overview-coachee',
@@ -14,18 +15,21 @@ export class SessionsOverviewCoacheeComponent implements OnInit {
 
   sessions!: Observable<Session[]>
   user!: Observable<User>;
+  currentUser!: User;
 
   constructor(private userService: UserService,
-              private sessionService: SessionService) { }
+              private sessionService: SessionService,
+              private keyCloakService: KeycloakService) { }
 
   ngOnInit(): void {
     this.sessions = this.sessionService.getAllSessions()
     // console.log(this.userService.getUser(id).subscribe())
+    this.userService.getUserBy(this.keyCloakService.getUsername()).subscribe(user =>
+      this.currentUser = user)
   }
 
   getUserFullName(id: string) {
     console.log(this.userService.getUser(id).subscribe(
     ));
   }
-
 }
