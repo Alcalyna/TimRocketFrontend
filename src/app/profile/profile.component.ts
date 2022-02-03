0b1fa8fe7f8716c77a63b71c83bc835c5578209b
AcidJu <70847984+AcidJu@users.noreply.github.com>
2022-02-03 11:46:16 +0100

    cleaning

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,21 +4,16 @@ import {KeycloakService} from "../keycloak/keycloak.service";
 import {Observable} from "rxjs";
 import {UserService} from "../../service/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-
 })
 export class ProfileComponent implements OnInit {
-
   currentUser!: User;
   userToDisplay!: User;
   loggedInUser!: boolean
   user!: Observable<User>;
-  userToDisplayId!: string | null;
-
   constructor(
     private keyCloakService: KeycloakService,
     private userService: UserService,
@@ -26,10 +21,8 @@ export class ProfileComponent implements OnInit {
     private router: Router
   ) {
   }
-
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    console.log("33 " + id);
     if (this.keyCloakService.isLoggedIn() && id == null) {
       this.userService.getUserBy(this.keyCloakService.getUsername())
         .subscribe(
@@ -37,31 +30,23 @@ export class ProfileComponent implements OnInit {
             this.currentUser = user, this.userToDisplay = user
           });
     } else {
-      this.userToDisplayId = id;
-      console.log("42 " + id);
-    }
-    this.getUser(this.userToDisplayId!);
-    if (this.keyCloakService.isLoggedIn() && id == JSON.parse(localStorage.getItem('loggedInUser')!).userId) {
-      console.log("46 " + id);
-      this.router.navigateByUrl('/profile');
+      if (this.keyCloakService.isLoggedIn() && id == JSON.parse(localStorage.getItem('loggedInUser')!).userId) {
+        this.router.navigateByUrl('/profile');
+      } else {
+        this.getUser(id!);
+      }
     }
   }
-
   isACoachee(): boolean {
     if (this.keyCloakService.isLoggedIn()) {
-      // console.log("The role is " + JSON.parse(localStorage.getItem('loggedInUser')!).role.toLowerCase());
       return JSON.parse(localStorage.getItem('loggedInUser')!).role.toLowerCase() === 'coachee';
     }
     return false;
   }
-
   getUser(id: string) {
     console.log("60 The id is " + id);
     this.userService.getUser(id!)
-      .subscribe(user => user == null && !(this.keyCloakService.isLoggedIn() && id == JSON.parse(localStorage.getItem('loggedInUser')!).userId) ? console.log("62") : this.userToDisplay = user
+      .subscribe(user => user == null ? this.router.navigateByUrl('') : this.userToDisplay = user
       );
   }
-
-  //this.router.navigateByUrl('')
-
 }
